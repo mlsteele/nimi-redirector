66f7a7329de4faa79b9694ac5fec9ce2dad663f7
Miles Steele <miles@milessteele.com>
2015-08-20 23:43:56 +0200

    add delete button

--- nimi.py
+++ nimi.py
@@ -19,6 +19,20 @@ def root():
   return flask.render_template("index.html",
                                routes=display_routes)
 
+@app.route("/", methods=["POST"])
+def modify():
+  if flask.request.form["action"] == "create":
+    slug = flask.request.form["slug"]
+    url = flask.request.form["url"]
+    routes[slug] = url
+    return flask.redirect("/")
+  if flask.request.form["action"] == "delete":
+    slug = flask.request.form["slug"]
+    del routes[slug]
+    return flask.redirect("/")
+
+  return flask.abort(400)
+
 @app.route("/<path:path>")
 def somewhere(path):
   if path in routes:
@@ -28,12 +42,6 @@ def somewhere(path):
     return flask.render_template("unmapped.html",
                                  slug=path,
                                  example=get_example_url())
-
-@app.route("/<path:path>", methods=["POST"])
-def create(path):
-  url = flask.request.form["url"]
-  routes[path] = url
-  return flask.redirect("/")
 
 def get_example_url():
   if random.random() < 0.1:
