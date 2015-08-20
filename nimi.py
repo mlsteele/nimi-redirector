81eb6c470c41a1c529c18b4aa8c71779cf694bd0
Miles Steele <miles@milessteele.com>
2015-08-21 00:36:52 +0200

    use mongodb to store routes

--- nimi.py
+++ nimi.py
@@ -1,43 +1,40 @@
-import flask
 import itertools
 import random
+import flask
+import flask.ext.pymongo
+
 app = flask.Flask(__name__)
+mongo = flask.ext.pymongo.PyMongo(app)
 
 EXAMPLE_WORDS = ["correct", "horse", "battery", "stapler"]
 
-routes = {
-  "findclass": "http://student.mit.edu/catalog/extsearch.cgi",
-  "mitpay": "https://student.mit.edu/cgi-bin/mitpay.pl",
-  "ist": "https://ist.mit.edu/software-hardware",
-  "so": "https://stackoverflow.com/",
-  "planner": "http://planner.mit.edu/",
-}
-
 @app.route("/")
 def root():
-  display_routes = itertools.islice(routes.iteritems(), 40)
+  display_routes = [(r["slug"], r["url"])
+                    for r in mongo.db.routes.find().limit(40)]
   return flask.render_template("index.html",
                                routes=display_routes)
 
 @app.route("/", methods=["POST"])
 def modify():
   if flask.request.form["action"] == "create":
-    slug = flask.request.form["slug"]
-    url = flask.request.form["url"]
-    routes[slug] = url
+    mongo.db.routes.insert({
+      "slug": flask.request.form["slug"],
+      "url": flask.request.form["url"],
+    })
     return flask.redirect("/")
   if flask.request.form["action"] == "delete":
     slug = flask.request.form["slug"]
-    del routes[slug]
+    mongo.db.routes.remove({"slug": slug})
     return flask.redirect("/")
 
   return flask.abort(400)
 
 @app.route("/<path:path>")
 def somewhere(path):
-  if path in routes:
-    url = routes[path]
-    return flask.redirect(url)
+  route = mongo.db.routes.find_one({"slug": path})
+  if route:
+    return flask.redirect(route["url"])
   else:
     return flask.render_template("unmapped.html",
                                  slug=path,
